af39d8b69e26350771b0662a33b3570cff48bbbf
andlsales95 <anderson.sales@technosoftware.com.br>
2021-04-21 10:50:09 -0300

    create project xy inc

---
+++ src/main/java/br/com/xyinc/xyincapi/XyincapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.xyinc.xyincapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyincapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyincapiApplication.class, args);
+	}
+
+}
